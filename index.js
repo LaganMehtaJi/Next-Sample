c1c778a12cdc1609f6aadbcc5d2d5f4d2990efc1
LaganMehtaJi <laganmehta.in@gmail.com>
2025-01-14 13:24:42 +0530

    now remove all bugs in rest api in get by id

--- index.js
+++ index.js
@@ -6,9 +6,13 @@ const PORT = 8000;
 app.get('/api/user',(req,res)=>{
  return res.json(users);
 });
-app.get('/api/user/origin/:id',(req ,res)=>{
+app.get('/api/user/:id',(req ,res)=>{
    const id = Number(req.params.id);
-   const foundUser = users.find(user =>(user.id ===id ));
+   const foundUser = users.find(user =>(user.course_id ===id ));
+      
+   if (!foundUser) {
+    return res.status(404).json({ message: "User not found" });
+}
    return res.json(foundUser); 
 });
 app.listen(PORT ,() =>console.log(`Server started at :${PORT} `));
